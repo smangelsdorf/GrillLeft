7e909e4e01f12290f14d593b55ad602c24dee5da
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2017-03-12 06:01:19 +1000

    Add AveragingDateTimePoint model

---
+++ GrillLeft/Model/AveragingDateTimePoint.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrillLeft.Model
+{
+    internal class AveragingDateTimePoint
+    {
+        internal readonly double Total;
+        internal readonly int Count;
+        internal readonly DateTime DateTime;
+
+        internal AveragingDateTimePoint(DateTime dateTime, double value) : this(dateTime, value, 1)
+        {
+        }
+
+        internal AveragingDateTimePoint(DateTime dateTime, double total, int count)
+        {
+            Total = total;
+            Count = count;
+            DateTime = dateTime;
+        }
+
+        internal double Value
+        {
+            get
+            {
+                return Total / Count;
+            }
+        }
+
+        internal AveragingDateTimePoint Concat(AveragingDateTimePoint other)
+        {
+            var dateTime = DateTime;
+            if (other.DateTime > DateTime) dateTime = other.DateTime;
+
+            var newTotal = Total + other.Total;
+            var newCount = Count + other.Count;
+
+            return new AveragingDateTimePoint(dateTime, newTotal, newCount);
+        }
+    }
+}
---
+++ GrillLeftTests/Model/AveragingDateTimePointTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GrillLeft.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrillLeft.Model.Tests
+{
+    [TestClass()]
+    public class AveragingDateTimePointTests
+    {
+        [TestMethod()]
+        public void ValueTest()
+        {
+            var rand = new Random();
+
+            var time = new DateTime(rand.Next());
+            var total = rand.NextDouble() * 1e10;
+            var count = rand.Next();
+
+            var point = new AveragingDateTimePoint(time, total, count);
+            Assert.AreEqual(total / count, point.Value);
+        }
+
+        [TestMethod()]
+        public void ConcatTest()
+        {
+            var rand = new Random();
+
+            var time1 = new DateTime(rand.Next());
+            var total1 = rand.NextDouble() * 1e10;
+            var count1 = rand.Next();
+            var point1 = new AveragingDateTimePoint(time1, total1, count1);
+
+            var time2 = new DateTime(rand.Next());
+            var total2 = rand.NextDouble() * 1e10;
+            var count2 = rand.Next();
+            var point2 = new AveragingDateTimePoint(time2, total2, count2);
+
+            var point = point1.Concat(point2);
+            Assert.AreEqual(new DateTime[] { time1, time2 }.Max(), point.DateTime);
+            Assert.AreEqual(total1 + total2, point.Total);
+            Assert.AreEqual(count1 + count2, point.Count);
+        }
+    }
+}
